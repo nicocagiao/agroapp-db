66d9f5a1c1847515259f57e4397f36174375badf
Nico <nicolascagiao@hotmail.com>
2021-02-22 14:50:33 -0300

    base de datos

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'admin',
+      database : 'agroapp'
+    },
+},
+};
