31d94deff7662e313eaa339c18e8a332231c9a26
Nico <nicolascagiao@hotmail.com>
2021-02-25 16:39:12 -0300

    knex connection

--- knexfile.js
+++ knexfile.js
@@ -11,11 +11,7 @@ module.exports = {
     },
     production: {
       client: 'pg',
-    connection: {
-      host : 'ec2-3-209-176-42.compute-1.amazonaws.com',
-      user : 'bsmrjohmufeusc',
-      password : '9620bd8d41c92a54af742787fa9be380334e19f62b4ca5b463a5125a40ef6e8e',
-      database : process.env.DATABASE_URL
+      connection: process.env.DATABASE_URL,
 },
 },
-}};
+};
